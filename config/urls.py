4c0a46bd97ef7bd497bfc7231846dd656ce82255
Marabrie <mararbrie@gmail.com>
2021-10-15 09:42:04 -0400

    working on adding photos and logo, getting them to appear on screen

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -27,6 +28,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
     path('', include('kafkan_store.urls')),
-]
+    ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
