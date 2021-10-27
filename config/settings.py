692fef1e1a869585a8df8455c3f5deafeed79ab9
Marabrie <mararbrie@gmail.com>
2021-10-27 19:44:21 -0400

    changed the media root

--- config/settings.py
+++ config/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = [BASE_DIR / "media/media"]
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/media")
 MEDIA_URL = "/media/"
 
 LOGIN_REDIRECT_URL = 'kafkan_list'
