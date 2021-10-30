ff7e47083b78641f0e634ecbf7bd32c5e1696a3e
Marabrie <mararbrie@gmail.com>
2021-10-30 11:14:04 -0400

    trying to get images from AWS to heroku

--- config/settings.py
+++ config/settings.py
@@ -130,15 +130,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-# MEDIA_URL = "/media/"
+MEDIA_URL = "/media/"
 
 LOGIN_REDIRECT_URL = 'kafkan_list'
 LOGOUT_REDIRECT_URL = 'kafkan_list'
@@ -156,8 +156,8 @@ del DATABASES['default']['OPTIONS']['sslmode']
 # AWS Setup
 AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'kafkan-static'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+S3_BUCKET_NAME = 'kafkan-static'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % S3_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
